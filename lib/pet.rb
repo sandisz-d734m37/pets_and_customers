19608c923b75a7f6f705ac09da721e5cf80178ba
Sandisz Thieme <sandiszthieme@gmail.com>
2022-02-15 19:26:12 -0700

    Feat: give Pet ability to feed

--- lib/pet.rb
+++ lib/pet.rb
@@ -5,5 +5,17 @@ class Pet
     @name = args[0].values[0]
     @type = args[0].values[1]
     @age = args[0].values[2]
+    @fed = false
   end
+
+  def fed?
+    @fed
+  end
+
+  def feed
+    if @fed == false
+      @fed = true
+    end
+  end
+
 end
