2fbd69564ce0927cd4b396081b2b5b10e3901c58
Sandisz Thieme <sandiszthieme@gmail.com>
2022-02-15 21:10:09 -0700

    Feat: Give Groomer ability to check which customers owe dues

--- lib/groomer.rb
+++ lib/groomer.rb
@@ -1,13 +1,26 @@
 class Groomer
-  attr_reader :name
+  attr_reader :name, :customers_with_dues
   attr_accessor :customers
   def initialize(name)
     @name = name
     @customers = []
+    @customers_with_dues = []
   end
 
   def custy_acquire(custy)
     customers << custy
+  end
+
+  def check_balances
+    customers.each_with_index do |custy, index|
+      unless @customers_with_dues.include?(custy) || custy.outstanding_balance == 0
+        @customers_with_dues << custy
+      end
+      if custy.outstanding_balance == 0
+        @customers_with_dues.delete_at(index)
+      end
+    end
+    @customers_with_dues
   end
 
 end
--- spec/groomer_spec.rb
+++ spec/groomer_spec.rb
@@ -15,11 +15,25 @@ describe Groomer do
 
   bobby = Customer.new("Bobby", 0)
   hendrixx = Customer.new("Future Hendrix", 1)
-  
+
   it "can get a new customer" do
     expect(marco.customers).to eq([])
     marco.custy_acquire(bobby)
     marco.custy_acquire(hendrixx)
     expect(marco.customers).to eq([bobby, hendrixx])
+  end
+
+  it "can tell which customers owe a balance" do
+    expect(marco.check_balances).to eq([])
+    bobby.charge(17)
+    # binding.pry
+    expect(marco.check_balances).to eq([bobby])
+  end
+
+  it "can also tell when a balance is paid off" do
+    hendrixx.charge(30)
+    expect(marco.check_balances).to include(hendrixx)
+    hendrixx.pay(30)
+    expect(marco.check_balances).not_to include(hendrixx)
   end
 end
